824d6808a25692eace4a29c2162a202f06178dec
genesislab-ryan <st.kim@genesislab.ai>
2022-04-01 16:45:06 +0900

    Update User objects
    다음 유저 객체의 사항들이 업데이트 되었습니다.
    - 랩탑 객체를 사용한 리스트 변수 초기화 (델 노트북, 맥북만 받는 리스트 생성)
    - 노트북 양도하기 함수 정리 (불필요한 반복 제거)

--- oop/src/main/user/User.java
+++ oop/src/main/user/User.java
@@ -1,7 +1,6 @@
 package main.user;
 
-import main.computer.laptop.DellNotebook;
-import main.computer.laptop.Macbook;
+import main.computer.laptop.Laptop;
 
 import java.util.List;
 
@@ -13,7 +12,7 @@ public class User {
     private String phoneNumber;
     private String birthDate;
     private String password;
-    private List<String> laptopIdList;
+    private List<Laptop> laptopIdList;
 
     public User (String userName, String userId, String address, String phoneNumber, String birthDate, String password) { // 생성자 초기화
         this.userName = userName;
@@ -24,27 +23,18 @@ public class User {
         this.password = password;
     }
 
-    public User (String userName, String userId, String address, String phoneNumber, String birthDate, String password, List<String> laptopIdList) { // 생성자 초기화
-        this.userName = userName;
-        this.userId = userId;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.birthDate = birthDate;
-        this.password = password;
-        this.laptopIdList = laptopIdList;
-    }
     public String getUserName() {return userName;}
 
     public String getUserId() {return userId;}
 
     public String getPassword() {return password;}
 
-    public List<String> getLaptopIdList() {
+    public List<Laptop> getLaptopIdList() {
         return laptopIdList;
     }
 
-    public void setLaptopIdList(String laptopId) {
-        laptopIdList.add(laptopId);
+    public void setLaptopIdList(Laptop laptop) {
+        laptopIdList.add(laptop);
     }
 
     public void showRegisteredUserInfo (){
@@ -54,22 +44,17 @@ public class User {
                 "생년월일 : " + birthDate + "\n");
     }
 
-    public void registerMyLaptop(String laptopId){
-        if (laptopIdList.contains(laptopId)){
+    public void registerMyLaptop(Laptop laptop){
+        if (laptopIdList.contains(laptop)){
             System.out.println("이미 소유하고 있는 랩탑입니다.");
         }else{
-            laptopIdList.add(laptopId);
+            laptopIdList.add(laptop);
             System.out.println(userName + "님의 랩탑으로 등록되었습니다.");
         }
     }
 
-    public void handOverLaptop(String laptopId, Macbook macbook){
-        laptopIdList.remove(laptopId);
-        macbook.initializeRegisteredUser(userId, password);
-    }
-
-    public void handOverLaptop(String laptopId, DellNotebook dellNotebook){
-        laptopIdList.remove(laptopId);
-        dellNotebook.initializeRegisteredUser(userId, password);
+    public void handOverLaptop(Laptop laptop){
+        laptopIdList.remove(laptop);
+        laptop.initializeRegisteredUser(userId, password);
     }
 }
