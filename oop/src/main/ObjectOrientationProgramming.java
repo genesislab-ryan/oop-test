afa642a2b19e8de048ea450dabe76f8f3a2e2b94
genesislab-ryan <st.kim@genesislab.ai>
2022-04-01 16:47:45 +0900

    Refactoring main function
    메인 함수에서 다음 사항들이 업데이트 되었습니다.
    - 함수 모듈화
    - 불필요한 변수 할당 제거

--- oop/src/main/ObjectOrientationProgramming.java
+++ oop/src/main/ObjectOrientationProgramming.java
@@ -1,5 +1,8 @@
 package main;
 
+import main.computer.laptop.Laptop;
+import main.computer.laptop.Macbook;
+import main.computer.laptop.Portable;
 import main.user.User;
 
 import java.io.IOException;
@@ -10,61 +13,72 @@ import java.util.Scanner;
 
 public class ObjectOrientationProgramming {
 
+    public static void messageInfo() {
+        System.out.println("수행할 기능을 선택해주세요.");
+        System.out.println("1 : 사용자 객체 정보 입력");
+        System.out.println("2 : 기존 사용자로 접속");
+        System.out.println("3 : 사용자 정보 조회");
+        System.out.println("4 : 사용자 노트북 등록");
+        System.out.println("5 : 사용자 노트북 양도");
+        System.out.println("6 : 랩탑 실행");
+        System.out.println("7 : 랩탑 종료");
+        System.out.println("8 : 랩탑 시스템 정보 출력");
+        System.out.println("9 : 랩탑 시스템 사용량 출력");
+        System.out.println("10 : 랩탑에 설치된 애플리케이션 조회");
+        System.out.println("11 : 신규 애플리케이션 설치");
+        System.out.println("12 : 애플리케이션 실행");
+        System.out.println("13 : 애플리케이션 종료");
+        System.out.println("14 : 애플리케이션 삭제");
+        System.out.println("-1 : 프로그램 종료");
+    }
+
+    public static User registerUserInfo(Scanner scanner){
+        System.out.println("사용자 정보를 입력해주세요.");
+
+        System.out.println("이름 : ");
+        String userName = scanner.next();
+
+        System.out.println("ID : ");
+        String userId = scanner.next();
+
+        System.out.println("주소 : ");
+        String address = scanner.next();
+
+        System.out.println("전화번호 : ");
+        String phoneNumber = scanner.next();
+
+        System.out.println("생년월일 : ");
+        String birthDate = scanner.next();
+
+        System.out.println("비밀번호 : ");
+        String password = scanner.next();
+
+        User user = new User(userName, userId, address, phoneNumber, birthDate, password);
+
+        return user;
+    }
+
     public static void main(String[] args) throws IOException {
         List<User> allRegisteredUserList = new ArrayList<>(); // 등록한 User 정보
-        List<Object> allLaptopList = new ArrayList<>(); // 전체 노트북 리스트 (dell, macbook)
+        List<Laptop> allLaptopList = new ArrayList<>(); // 전체 노트북 리스트 (dell, macbook)
         HashMap<String, User> allRegisteredLaptopList = new HashMap<>(); // 등록된 노트북 : laptop ID, 유저 정보
-        User currentUser = new User(null, null, null, null, null, null, new ArrayList<>());; // 현재 접속 중인 사용자 (초기화 값은 null)
+        User currentUser = null; // 현재 접속 중인 사용자 (초기화 값은 null)
         Boolean onOff = true;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("oop test 프로그램입니다.");
         while (onOff){
-            System.out.println("수행할 기능을 선택해주세요.");
-            System.out.println("1 : 사용자 객체 정보 입력");
-            System.out.println("2 : 기존 사용자로 접속");
-            System.out.println("3 : 사용자 정보 조회");
-            System.out.println("4 : 사용자 노트북 등록");
-            System.out.println("5 : 사용자 노트북 양도");
-            System.out.println("6 : 랩탑 실행");
-            System.out.println("7 : 랩탑 종료");
-            System.out.println("8 : 랩탑 시스템 정보 출력");
-            System.out.println("9 : 랩탑 시스템 사용량 출력");
-            System.out.println("10 : 랩탑에 설치된 애플리케이션 조회");
-            System.out.println("11 : 신규 애플리케이션 설치");
-            System.out.println("12 : 애플리케이션 실행");
-            System.out.println("13 : 애플리케이션 종료");
-            System.out.println("14 : 애플리케이션 삭제");
-            System.out.println("-1 : 프로그램 종료");
 
-            String number = scanner.next();
+            messageInfo();
+
+            Integer number = scanner.nextInt();
             switch (number){
-                case "1": // 사용자 객체 정보 입력
-                    System.out.println("사용자 정보를 입력해주세요.");
-
-                    System.out.println("이름 : ");
-                    String userName = scanner.next();
-
-                    System.out.println("ID : ");
-                    String userId = scanner.next();
-
-                    System.out.println("주소 : ");
-                    String address = scanner.next();
-
-                    System.out.println("전화번호 : ");
-                    String phoneNumber = scanner.next();
-
-                    System.out.println("생년월일 : ");
-                    String birthDate = scanner.next();
-
-                    System.out.println("비밀번호 : ");
-                    String password = scanner.next();
-
-                    User user = new User(userName, userId, address, phoneNumber, birthDate, password);
-                    currentUser = user;
-                    allRegisteredUserList.add(user);
-                    user.showRegisteredUserInfo();
+                case 1: // 사용자 객체 정보 입력
+                    currentUser = registerUserInfo(scanner);
+                    allRegisteredUserList.add(currentUser);
+                    currentUser.showRegisteredUserInfo();
                     break;
-                case "2": // 기존 사용자로 접속
+                case 2: // 기존 사용자로 접속
                     while (true){
                         if (allRegisteredUserList.isEmpty()){
                             System.out.println("등록되어 있는 유저가 존재하지 않습니다.");
@@ -89,7 +103,7 @@ public class ObjectOrientationProgramming {
                         }
                         break;
                     }
-                case "3": // 사용자 정보 조회
+                case 3: // 사용자 정보 조회
                     if (currentUser.getUserId() == null){
                         System.out.println("로그인을 먼저 해주세요.");
                         break;
@@ -97,9 +111,8 @@ public class ObjectOrientationProgramming {
                         currentUser.showRegisteredUserInfo();
                         break;
                     }
-                case "4": // 사용자 노트북 등록
-
-                    if (currentUser.getUserId() == null){
+                case 4: // 사용자 노트북 등록
+                    if (currentUser == null){
                         System.out.println("노트북 등록 전, 로그인을 먼저 수행해주세요.");
                         break;
                     }
@@ -120,10 +133,10 @@ public class ObjectOrientationProgramming {
                         System.out.println("이미 등록된 노트북이 존재합니다.");
                     }
                     break;
-                case "5": // 다른 사용자에게 노트북 양도하기
+                case 5: // 다른 사용자에게 노트북 양도하기
 
                     break;
-                case "-1": // 프로그램 종료
+                case -1: // 프로그램 종료
                     System.out.println("프로그램을 종료합니다.");
                     onOff = false;
                     break;
