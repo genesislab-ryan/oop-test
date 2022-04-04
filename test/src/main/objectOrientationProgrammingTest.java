2b858e9f5bbd7d1cc921d57727945d9f1412c3cb
genesislab-ryan <st.kim@genesislab.ai>
2022-04-04 14:25:31 +0900

    Update test codes
    다음 테스트 코드가 업데이트 되었습니다.
    - User test code
    - laptop test code

--- test/src/main/objectOrientationProgrammingTest.java
+++ test/src/main/objectOrientationProgrammingTest.java
@@ -1,7 +1,7 @@
 package main;
-
 public class objectOrientationProgrammingTest {
 
     public static void main (String[] args){
+
     }
 }
--- test/src/main/user/UserTest.java
+++ test/src/main/user/UserTest.java
@@ -23,8 +23,6 @@ class UserTest {
     private DellNotebook dellNotebook;
     private Integer userIdCount = 0;
     private User user;
-    private User macbookUser;
-    private User dellNotebookUser;
     private User allLaptopUser;
 
     @BeforeEach
@@ -39,26 +37,7 @@ class UserTest {
         dellNotebook = new DellNotebook("dell", "d4c3b2a1", "XPS");
         userIdCount = 0;
         user = new User(userName, ++userIdCount, address, phoneNumber, birthDate, password);
-        macbookUser = new User(userName, ++userIdCount, address, phoneNumber, birthDate, password);
-        dellNotebookUser = new User(userName, ++userIdCount, address, phoneNumber, birthDate, password);
         allLaptopUser = new User(userName, ++userIdCount, address, phoneNumber, birthDate, password);
-
-    }
-
-    @AfterEach
-    public void teardown() throws Exception{
-    }
-
-
-    @Test
-    @DisplayName("생성한 유저 정보 가져오기")
-    public void registeredUserInfoTest() {
-
-        assertEquals(user.getUserName(), "tester");
-        assertEquals(user.getUserId(), (Integer)  1);
-        assertEquals(user.getAddress(), "울릉도 동남쪽 뱃길따라 이백리");
-        assertEquals(user.getBirthDate(), "1999-12-12");
-        assertEquals(user.getPassword(), "1234");
 
     }
 
@@ -66,42 +45,52 @@ class UserTest {
     @DisplayName("생성한 유저 정보에 노트북 등록하기")
     public void registerMyLaptopTest() {
 
-        macbookUser.registerMyLaptop(macbook);
-        dellNotebookUser.registerMyLaptop(dellNotebook);
+        user.registerMyLaptop(macbook);
+        user.registerMyLaptop(macbook); // 중복 등록 방지 로직이 수행되는지 확인하기 위한 코드
+
+        allLaptopUser.registerMyLaptop(macbook);
         allLaptopUser.registerMyLaptop(macbook);
         allLaptopUser.registerMyLaptop(dellNotebook);
 
         List<Laptop> macbookList = Arrays.asList(macbook);
-        List<Laptop> dellNotebookList = Arrays.asList(dellNotebook);
         List<Laptop> allLaptopList = Arrays.asList(macbook, dellNotebook);
 
-        assertEquals(macbookUser.getLaptopList(), macbookList); // 맥북만 등록되었을 때
-        assertEquals(dellNotebookUser.getLaptopList(), dellNotebookList); // 델 노트북만 등록되었을 때
+        assertEquals(user.getLaptopList(), macbookList); // 맥북만 등록되었을 때
+        assertEquals(user.getLaptopList().size(), macbookList.size()); // 동일 맥북 등록 여부 확인 (동일 노트북 추가 로직 실행 시, )
+
         assertEquals(allLaptopUser.getLaptopList(), allLaptopList); // 모든 종류의 노트북이 다 등록되었을 때
+        assertEquals(allLaptopUser.getLaptopList().size(), allLaptopList.size());
 
     }
 
-//    @Test
-//    @DisplayName("사용자 객체에 등록된 노트북 양도하기")
-//    public void handOverLaptopTest() {
-//        List<Laptop> macbookList = new ArrayList<>(Arrays.asList(macbook));
-//        List<Laptop> dellNotebookList = new ArrayList<>(Arrays.asList(dellNotebook));
-//        List<Laptop> emptyLaptopList = new ArrayList<>();
-//
-//        macbook.setRegisteredUser(user);
-//        dellNotebook.setRegisteredUser(user);
-//
-//        macbook.initializeRegisteredUser(userId, password);
-//        macbookList.remove(macbook);
-//
-//        dellNotebook.initializeRegisteredUser(userId, password);
-//        dellNotebookList.remove(dellNotebook);
-//
-//        assertEquals(macbook.getRegisteredUser(), null);
-//        assertEquals(dellNotebook.getRegisteredUser(), null);
-//
-//        assertEquals(macbookList, emptyLaptopList);
-//        assertEquals(dellNotebookList, emptyLaptopList);
-//    }
+    @Test
+    @DisplayName("사용자 객체에 등록된 노트북 양도하기")
+    public void handOverLaptopTest() {
+        List<Laptop> macbookList = new ArrayList<>(Arrays.asList(macbook));
+        List<Laptop> dellNotebookList = new ArrayList<>(Arrays.asList(dellNotebook));
+        List<Laptop> emptyLaptopList = new ArrayList<>();
+
+        User handOverUser = new User("handOver", 3, "백령도 앞바다", "000-0000-0000", "1231", "1234");
+
+        macbook.setRegisteredUser(user);
+        dellNotebook.setRegisteredUser(user);
+
+        macbook.initializeRegisteredUser(userId, password);
+        macbookList.remove(macbook);
+
+        dellNotebook.initializeRegisteredUser(userId, password);
+        dellNotebookList.remove(dellNotebook);
+
+        // 양도하는 유저에 대한 함수 코드 구현
+        handOverUser.registerMyLaptop(macbook);
+        handOverUser.registerMyLaptop(dellNotebook);
+
+        assertEquals(macbook.getRegisteredUser(), null);
+        assertEquals(dellNotebook.getRegisteredUser(), null);
+
+        assertEquals(macbookList, emptyLaptopList);
+        assertEquals(dellNotebookList, emptyLaptopList);
+        assertTrue(handOverUser.getLaptopList().contains(macbook));
+    }
 
 }
